cf91b7efd9689d44b88f96b3c11b2b4d6944bca1
Kaloyan73 <kaloyan.hristov_37@abv.bg>
2024-09-25 20:51:04 +0300

    Implemented Search from functionality

--- solution.js
+++ solution.js
@@ -18,6 +18,25 @@ function changeContent(className) {
 
 document.querySelector('#new-reservation').addEventListener('click', (e) => cleanData(e));
 
+changeContent('search-form-content');
+document.querySelector('#search-form-button').addEventListener('click', (e) => searchFormData(e));
+
+function searchFormData(e) {
+    e.preventDefault();
+    const data = e.target.parentElement;
+    const checkIn = data.querySelector('#check-in').value;
+    const checkOut = data.querySelector('#check-out').value;
+    const people = data.querySelector('#people').value;
+    if (checkIn != '' && checkOut != '' && people != '' &&
+        new Date(checkIn) <= new Date(checkOut)) {
+        reservation.startDate = checkIn;
+        reservation.endDate = checkOut;
+        reservation.guestsCount = people;
+        console.log(reservation);
+        changeContent('search-result-form-content');
+    }
+}
+
 function cleanData(e) {
     changeContent('search-form-content');
 }
